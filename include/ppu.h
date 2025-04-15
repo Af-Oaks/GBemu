61864aa5d22d50b005066df6b4faf83e81995a38
Af-Oaks <filho.freiretn@gmail.com>
2025-04-15 08:45:52 -0300

    feat: WIP ppu

--- include/ppu.h
+++ include/ppu.h
@@ -2,5 +2,14 @@
 
 #include <common.h>
 
+typedef struct 
+{
+    u8 vram[0x2000];
+}ppu_context;
+
+
+
 void ppu_init();
 void ppu_tick();
+void ppu_vram_write(u16 address, u8 value);
+u8 ppu_vram_read(u16 address);
--- lib/ppu.c
+++ lib/ppu.c
@@ -1,9 +1,19 @@
 #include <ppu.h>
 
+static ppu_context ctx;
+
 void ppu_init() {
 
 }
 
 void ppu_tick() {
 
+}
+
+void ppu_vram_write(u16 address, u8 value) {
+    ctx.vram[address - 0x8000] = value;
+}
+
+u8 ppu_vram_read(u16 address) {
+    return ctx.vram[address - 0x8000];
 }
