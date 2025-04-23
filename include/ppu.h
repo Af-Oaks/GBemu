811a760b5bd6559ce5bfe0563a44081b1ca8349a
Af-Oaks <filho.freiretn@gmail.com>
2025-04-23 10:31:30 -0300

    feat: WIP ppu

--- include/ppu.h
+++ include/ppu.h
@@ -2,7 +2,10 @@
 
 #include <common.h>
 
-
+static const int LINES_PER_FRAME = 154;
+static const int TICKS_PER_LINE = 456;
+static const int YRES = 144;
+static const int XRES = 160;
 
 typedef struct 
 {
@@ -16,6 +19,54 @@ typedef struct
     u8 f_y_flip : 1;
     u8 f_bgp : 1;
 }oam_entry;
+
+typedef enum 
+{
+   FS_GET_TILE,
+   FS_GET_TILE_LOW,
+   FS_GET_TILE_HIGH,
+   SLEEP,
+   FS_PUSH
+} FETCHER_STATE;
+
+typedef struct 
+{
+    u8 color;
+    u8 pallet;
+    u8 sp_priority;
+    u8 bg_priority;
+}fifo_pixel;
+
+
+typedef struct _fifo_entry
+{
+    struct _fifo_entry * next;
+    fifo_pixel fifo_pixel;
+}fifo_entry;
+
+typedef struct 
+{
+    fifo_entry *back;
+    fifo_entry *front;
+    u32 size;
+}fifo;
+
+typedef struct 
+{
+    fifo_pixel fifo_pixel;
+    oam_entry oam_entry;
+    FETCHER_STATE fs_state;// one is enough?
+    u8 line_x;
+    u8 fetch_x;//hm....
+    u8 push_x;
+    u8 line_y;
+    u8 fifo_number;
+    
+
+
+}pixel_context;
+
+
 
 typedef struct 
 {
